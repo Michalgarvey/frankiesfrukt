f1e4677d51ed44ca6b807bf10cb38a826538aff0
Michal Garvey <michalgarvey@gmail.com>
2017-12-21 17:11:18 +0100

    4.3 Controllers

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,3 +2,11 @@ class SimplePagesController < ApplicationController
   def other
   end
 end
+
+
+landing_page
+class SimplePagesController < ApplicationController
+  def landing_page
+    @featured_product = Product.first
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'simple_pages/landing_page'
+
   resources :products
   get 'simple_pages/other'
 
@@ -7,7 +9,9 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
