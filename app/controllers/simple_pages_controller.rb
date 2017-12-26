35ecc0a3df531640ecc122f01f31e6d731751435
Michal Garvey <michalgarvey@gmail.com>
2017-12-26 14:38:58 +0100

    updates to 4.2

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,4 @@
-class SimplePagesController < ApplicationController
-  def other
-  end
-end
 
-
-landing_page
 class SimplePagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
