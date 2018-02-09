c4bd9245332cde2f495512489fd66377b928a984
Michal Garvey <michalgarvey@gmail.com>
2018-02-09 16:39:40 +0100

    6.3.2 corrections to user tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,42 +1,39 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-    let(:user) { User.create!(email: "test@test.com", password: "password")}
-    let(:user2) { User.create!(email: "2test@test.com", password: "2password")}
+  before do
+    @user = User.create!(email: "test@test.com", password: "password")
+    @user2 = User.create!(email: "2test@test.com", password: "2password")
+  end
 
     describe 'GET #show' do
 
       context 'when a user is logged in' do
         before do
-          sign_in user
-        end
-        it 'loads correct user details' do
-          get :show, params: { id: user.id }
-          expect(response).to be_ok
-          expect(assigns(:user)).to eq user
+
+          it 'loads correct user details' do
+
+            expect(response).to have_http_status(200)
+            expect(assigns(:user)).to eq @user
+          end
         end
       end
 
+
       context 'when a user is not logged in' do
         it 'redirects to login' do
-          get :show, params: { id: user.id }
+          get :show, params: { id: @user.id }
           expect(response).to redirect_to(new_user_session_path)
         end
       end
 
       context 'User cannot access user2s details' do
-        before do
-          sign_in user
-        end
         it 'loads correct user details' do
-          get :show, params: { id: user2.id }
+          get :show, params: { id: @user2.id }
           expect(response).to redirect_to(root_path)
-          expect(response).to have_http_status(302)
+
         end
       end
-
-
-
     end
 
 end
