7a9835e17d2b96bd94aa4ced0c18c577f01be89d
Michal Garvey <michalgarvey@gmail.com>
2018-02-12 14:59:45 +0100

    6.4 Stripe

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,10 @@
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key: ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    publishable_key: 'pk_test_s1FIUptIaoivPYQ0cgRytIce',
+    secret_key: 'sk_test_5PC4MaeswZqRxs1f6teJ1apK'
+end
