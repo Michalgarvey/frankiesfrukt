c4650ead9f41c8789d8020f1b0c5f89048d2d0fe
Michal Garvey <michalgarvey@gmail.com>
2018-02-20 16:56:18 +0100

    6.10.1 AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @product, notice: "Thanks, your review was created successfully!" }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
         format.html { redirect_to @product, alert: "Oops, there was an issue and your review has not been saved, please try again." }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
